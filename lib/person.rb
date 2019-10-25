cfc6e3ca78a509bbdc7a6dcda81f9714d5fafa6b
Emery Tate <tateejt@gmail.com>
2019-10-25 18:47:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   #your code here
   
-  attr_accessor :key, :value
+  attr_accessor :name, :key, :value
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
